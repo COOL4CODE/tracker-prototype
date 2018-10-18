f146a3eedd78c37991fcfb0f37c37be7cc48ae3f
azarateo <azarate@rochester.edu.co>
2018-10-18 04:46:10 -0500

    Commit incial del app

---
+++ tracker.js
@@ -0,0 +1,48 @@
+// load our app server using express...
+
+const express = require('express');
+const app     = express();
+const morgan  = require('morgan');
+const mysql   = require('mysql');
+var cors      = require('cors');
+
+// Prevent Cors-domain errors and allow it
+app.use(cors());
+
+// Logs customization
+app.use(morgan('combined'))
+
+app.get( "/", (req, res) => {
+    console.log(LOG_TAG + "Responding to root route");
+    res.send("¡Welcome! Hello from RCIS :)");
+
+})
+
+app.get("/asset/:id", (req, res) => {
+
+    
+})
+
+app.get("/person/:id", (req, res) => {
+   
+})
+
+ app.get("/people", (req, res) =>{
+ 
+ })
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
+var server = app.listen(3000, () => {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Server running at http://%s:%s', host, port);
+    console.log('🖖 C4C');
+})
